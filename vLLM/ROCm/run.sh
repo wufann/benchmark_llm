343969cfca8e797e21c7301c1e2a9e7f7e33dbb3
wufann <715544327@qq.com>
2024-11-21 15:51:50 +0800

    fix(vLLM): rearrange test loop

--- vLLM/ROCm/run.sh
+++ vLLM/ROCm/run.sh
@@ -1,39 +1,7 @@
 #!/usr/bin/bash
-#./vllm_benchmark_report.sh -s all -m meta-llama/Meta-Llama-3.1-8B-Instruct   -g 1 -d float16
-#./vllm_benchmark_report.sh -s all -m meta-llama/Meta-Llama-3.1-70B-Instruct  -g 8 -d float16
-#./vllm_benchmark_report.sh -s all -m meta-llama/Meta-Llama-3.1-405B-Instruct -g 8 -d float16
-#./vllm_benchmark_report.sh -s all -m meta-llama/Llama-2-7b-chat-hf           -g 1 -d float16
-#./vllm_benchmark_report.sh -s all -m meta-llama/Llama-2-70b-chat-hf          -g 8 -d float16
-#./vllm_benchmark_report.sh -s all -m mistralai/Mixtral-8x7B-Instruct-v0.1    -g 8 -d float16
-#./vllm_benchmark_report.sh -s all -m mistralai/Mixtral-8x22B-Instruct-v0.1   -g 8 -d float16
-#./vllm_benchmark_report.sh -s all -m mistralai/Mistral-7B-Instruct-v0.3      -g 1 -d float16
-#./vllm_benchmark_report.sh -s all -m Qwen/Qwen2-7B-Instruct                  -g 1 -d float16
-#./vllm_benchmark_report.sh -s all -m Qwen/Qwen2-72B-Instruct                 -g 8 -d float16
-#./vllm_benchmark_report.sh -s all -m core42/jais-13b-chat                    -g 1 -d float16
-#./vllm_benchmark_report.sh -s all -m core42/jais-30b-chat-v3                 -g 1 -d float16
+export PYTORCH_TUNABLEOP_ENABLED=1
+./vllm_benchmark_report.sh -s latency -m /models/Llama-2-7b-FP8-quark/ -g 1 -d float8 2>&1 | tee log_7b_fp8_tp1
+./vllm_benchmark_report.sh -s latency -m /models/Llama-2-7b-FP8-quark/ -g 2 -d float8 2>&1 | tee log_7b_fp8_tp2
 
-./vllm_benchmark_report.sh -s latency -m meta-llama/Meta-Llama-3.1-8B-Instruct   -g 1 -d float16
-./vllm_benchmark_report.sh -s latency -m meta-llama/Meta-Llama-3.1-70B-Instruct  -g 8 -d float16
-./vllm_benchmark_report.sh -s latency -m meta-llama/Meta-Llama-3.1-405B-Instruct -g 8 -d float16
-./vllm_benchmark_report.sh -s latency -m meta-llama/Llama-2-7b-chat-hf           -g 1 -d float16
-./vllm_benchmark_report.sh -s latency -m meta-llama/Llama-2-70b-chat-hf          -g 8 -d float16
-./vllm_benchmark_report.sh -s latency -m mistralai/Mixtral-8x7B-Instruct-v0.1    -g 8 -d float16
-./vllm_benchmark_report.sh -s latency -m mistralai/Mixtral-8x22B-Instruct-v0.1   -g 8 -d float16
-./vllm_benchmark_report.sh -s latency -m mistralai/Mistral-7B-Instruct-v0.3      -g 1 -d float16
-./vllm_benchmark_report.sh -s latency -m Qwen/Qwen2-7B-Instruct                  -g 1 -d float16
-./vllm_benchmark_report.sh -s latency -m Qwen/Qwen2-72B-Instruct                 -g 8 -d float16
-./vllm_benchmark_report.sh -s latency -m core42/jais-13b-chat                    -g 1 -d float16
-./vllm_benchmark_report.sh -s latency -m core42/jais-30b-chat-v3                 -g 1 -d float16
-#
-#./vllm_benchmark_report.sh -s throughput -m meta-llama/Meta-Llama-3.1-8B-Instruct   -g 1 -d float16
-#./vllm_benchmark_report.sh -s throughput -m meta-llama/Meta-Llama-3.1-70B-Instruct  -g 8 -d float16
-#./vllm_benchmark_report.sh -s throughput -m meta-llama/Meta-Llama-3.1-405B-Instruct -g 8 -d float16
-#./vllm_benchmark_report.sh -s throughput -m meta-llama/Llama-2-7b-chat-hf           -g 1 -d float16
-#./vllm_benchmark_report.sh -s throughput -m meta-llama/Llama-2-70b-chat-hf          -g 8 -d float16
-#./vllm_benchmark_report.sh -s throughput -m mistralai/Mixtral-8x7B-Instruct-v0.1    -g 8 -d float16
-#./vllm_benchmark_report.sh -s throughput -m mistralai/Mixtral-8x22B-Instruct-v0.1   -g 8 -d float16
-#./vllm_benchmark_report.sh -s throughput -m mistralai/Mistral-7B-Instruct-v0.3      -g 1 -d float16
-#./vllm_benchmark_report.sh -s throughput -m Qwen/Qwen2-7B-Instruct                  -g 1 -d float16
-#./vllm_benchmark_report.sh -s throughput -m Qwen/Qwen2-72B-Instruct                 -g 8 -d float16
-#./vllm_benchmark_report.sh -s throughput -m core42/jais-13b-chat                    -g 1 -d float16
-#./vllm_benchmark_report.sh -s throughput -m core42/jais-30b-chat-v3                 -g 1 -d float16
+./vllm_benchmark_report.sh -s latency -m /models/Llama-2-7b-chat-hf -g 1 -d float16 2>&1 | tee log_7b_fp16_tp1
+./vllm_benchmark_report.sh -s latency -m /models/Llama-2-7b-chat-hf -g 2 -d float16 2>&1 | tee log_7b_fp16_tp2
--- vLLM/ROCm/vllm_benchmark_report.sh
+++ vLLM/ROCm/vllm_benchmark_report.sh
@@ -87,15 +87,15 @@ if [ "$scenario" == "latency" ] || [ "$scenario" == "all" ]; then
             echo $model $mode $bat $tp $inp $out
             python3 $tool_latency --model $model --batch-size $bat -tp $tp --input-len $inp --output-len $out --num-iters-warmup $n_warm --num-iters $n_itr --trust-remote-code --output-json $outjson $DTYPE $OPTION_LATENCY_P
             python3 $tool_report --mode $mode --model $model_name --batch-size $bat --tp $tp --input-len $inp --output-len $out $dtype --input-json $outjson --output-csv $outcsv --dtype $datatype
-        done
-        # e2e latency
-        for out in $OutLatency;
-        do
-            outjson=${report_dir}/${model_name}_${mode}_decoding_bs${bat}_in${inp}_out${out}_${datatype}.json
-            outcsv=${report_summary_dir}/${model_name}_${mode}_report.csv
-            echo $model $mode $bat $tp $inp $out
-            python3 $tool_latency --model $model --batch-size $bat -tp $tp --input-len $inp --output-len $out --num-iters-warmup $n_warm --num-iters $n_itr --trust-remote-code --output-json $outjson $DTYPE $OPTION_LATENCY_D
-            python3 $tool_report --mode $mode --model $model_name --batch-size $bat --tp $tp --input-len $inp --output-len $out $dtype --input-json $outjson --output-csv $outcsv --dtype $datatype
+            # e2e latency
+            for out in $OutLatency;
+            do
+                outjson=${report_dir}/${model_name}_${mode}_decoding_bs${bat}_in${inp}_out${out}_${datatype}.json
+                outcsv=${report_summary_dir}/${model_name}_${mode}_report.csv
+                echo $model $mode $bat $tp $inp $out
+                python3 $tool_latency --model $model --batch-size $bat -tp $tp --input-len $inp --output-len $out --num-iters-warmup $n_warm --num-iters $n_itr --trust-remote-code --output-json $outjson $DTYPE $OPTION_LATENCY_D
+                python3 $tool_report --mode $mode --model $model_name --batch-size $bat --tp $tp --input-len $inp --output-len $out $dtype --input-json $outjson --output-csv $outcsv --dtype $datatype
+            done
         done
     done
 fi
