7a3f8e47678beb1f23201377bd43f3fbcd1bd5a4
wufann <715544327@qq.com>
2025-05-06 16:31:01 +0800

    modify script

--- SGLang/deepseek-r1/server.sh
+++ SGLang/deepseek-r1/server.sh
@@ -1,2 +1,5 @@
 # launch server
-python3 -m sglang.launch_server --model /path/to/DeepSeek-R1 --trust-remote-code --tp 8 --chunked-prefill-size 130172 --enable-torch-compile
+python3 -m sglang.launch_server --model /path/to/DeepSeek-R1 --trust-remote-code --tp 8
+
+## best performence
+#python3 -m sglang.launch_server --model /path/to/DeepSeek-R1 --trust-remote-code --tp 8 --chunked-prefill-size 130172 --enable-torch-compile
--- vLLM/ROCm/serving_benchmark/server.sh
+++ vLLM/ROCm/serving_benchmark/server.sh
@@ -3,5 +3,6 @@ unset VLLM_MOE_PADDING
 export VLLM_USE_TRITON_FLASH_ATTN=0
 export VLLM_USE_ROCM_FP8_FLASH_ATTN=1
 export VLLM_ROCM_USE_AITER=0
+# enable AITER is best
 #export VLLM_ROCM_USE_AITER=1
 vllm serve /path/to/model --enable-chunked-prefill False --trust-remote-code -tp 8 --swap-space 16 --disable-log-requests --dtype float16 --distributed-executor-backend mp
